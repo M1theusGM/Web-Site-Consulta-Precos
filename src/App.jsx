139ee66a46aff3b7586fe852b39138d6581ec067
MatheusM <110963875+M1theusGM@users.noreply.github.com>
2025-11-23 16:38:51 -0300

    Add redirect from '/' to '/login' in routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,25 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import Home from './pages/home';
 import Results from './pages/search/Results';
 import PriceHistory from './pages/product/PriceHistory.jsx';
 import Login from './pages/auth/Login.jsx';
 import Register from './pages/auth/Register.jsx';
 
-
 export default function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+
+      {/* Redireciona "/" para "/login" */}
+      <Route path="/" element={<Navigate to="/login" replace />} />
+
+      <Route path="/home" element={<Home />} />
       <Route path="/buscar" element={<Results />} />
       <Route path="/produto/:gid" element={<PriceHistory />} />
       <Route path="/login" element={<Login />} />
       <Route path="/entrar" element={<Login />} />
       <Route path="/registrar" element={<Register />} />
       <Route path="/signup" element={<Register />} />
+
     </Routes>
   );
 }
